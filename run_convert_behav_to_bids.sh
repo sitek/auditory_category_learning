68ad43c4f6a03ce98ca20b4bc99388055d71d8cc
krs228 <krs228@login0b.htc.sam.pitt.edu>
2022-08-09 16:39:39 -0400

    loop through all subjects

--- run_convert_behav_to_bids.sh
+++ run_convert_behav_to_bids.sh
@@ -3,4 +3,6 @@
 # --mem=2GB
 
 # convert psychopy outputs to BIDS format
-python convert_behav_to_bids.py --sub $1 --task $2
+for sub in FLT01 FLT02 FLT03 FLT04 FLT05 FLT06 FLT07 FLT09; do
+  python convert_behav_to_bids.py --sub $sub --task ToneLearning
+done

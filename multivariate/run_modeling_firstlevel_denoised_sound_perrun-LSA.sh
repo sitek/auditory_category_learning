74207c845f92dd78c5f5d69f14da643d25229cbf
Kevin Sitek <sitek.kevin@gmail.com>
2025-06-06 17:23:50 -0400

    Updating full-model first level modeling

--- multivariate/run_modeling_firstlevel_denoised_sound_perrun-LSA.sh
+++ multivariate/run_modeling_firstlevel_denoised_sound_perrun-LSA.sh
@@ -8,7 +8,7 @@ bidsroot=/bgfs/bchandrasekaran/krs228/data/FLT/data_denoised/
 
 python modeling_firstlevel_stimulus_perrun.py --sub=$1 --task=tonecat \
     --space=MNI152NLin2009cAsym --fwhm=0 \
-    --event_type=stimulus --model_type=LSA \
+    --event_type=sound --model_type=LSA \
     --t_acq=2 --t_r=3 \
     --bidsroot=$bidsroot \
     --fmriprep_dir=$bidsroot/derivatives/denoised_fmriprep-22.1.1/

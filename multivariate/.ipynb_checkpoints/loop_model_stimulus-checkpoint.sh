1efedd1979261286121abf5e8b7fd7fc7ddd9d8b
Kevin Sitek <sitek.kevin@gmail.com>
2025-06-06 17:30:04 -0400

    removing checkpoints

--- multivariate/.ipynb_checkpoints/loop_model_stimulus-checkpoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fwhm=1.50
-
-for subpath in /bgfs/bchandrasekaran/krs228/data/FLT/data_denoised/sub*/; do 
-  subid=$(basename $subpath)
-  echo $subid
-  sbatch run_modeling_firstlevel_denoised_stimulus-LSS.sh $subid
-done

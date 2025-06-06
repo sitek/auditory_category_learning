21cfae406dc28db94d09746c9ffba71a61a18ab2
Kevin Sitek <sitek.kevin@gmail.com>
2025-06-06 17:23:50 -0400

    Updating full-model first level modeling

--- multivariate/run_modeling_firstlevel_denoised_stimulus_perrun-LSS.sh
+++ multivariate/run_modeling_firstlevel_denoised_stimulus_perrun-LSS.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 #SBATCH --time=23:00:00
+#SBATCH --mem=32G
 
 #conda activate py3
+
+bidsroot=/bgfs/bchandrasekaran/krs228/data/FLT/data_denoised/
 
 python modeling_firstlevel_stimulus_perrun.py --sub=$1 --task=tonecat \
     --space=MNI152NLin2009cAsym --fwhm=0 \
     --event_type=stimulus --model_type=LSS \
     --t_acq=2 --t_r=3 \
-    --bidsroot=/bgfs/bchandrasekaran/krs228/data/FLT/data_denoised/ \
-    --fmriprep_dir=/bgfs/bchandrasekaran/krs228/data/FLT/data_denoised/derivatives/denoised_fmriprep-22.1.1/
+    --bidsroot=$bidsroot \
+    --fmriprep_dir=$bidsroot/derivatives/denoised_fmriprep-22.1.1/
